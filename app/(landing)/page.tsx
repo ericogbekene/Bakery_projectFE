fded4269caa01cbf4352fc3e2de81b58f5f62cb9
maetheartist <mariamkosemani1@gmail.com>
2025-03-07 16:14:44 +0100

    fix about us page.

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,12 @@
+import About from "@/components/about-us/About";
 import HeroSection from "@/components/ui/hero-section";
 
 export default function Home() {
   return (
     <>
-    <HeroSection/>
+    {/* <HeroSection/> */}
+    <About/>
+    
     </>
     
   );
---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import About from '@/components/about-us/About'
+import React from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div><About/></div>
+  )
+}
+
+export default page
