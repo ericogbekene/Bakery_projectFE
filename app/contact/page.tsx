e3e8364b63793244af98d300d26594c3875de398
maetheartist <mariamkosemani1@gmail.com>
2025-03-07 16:25:06 +0100

    fix about us page.

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Contact from '@/components/contact-us/Contact'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div><Contact/></div>
+  )
+}
+
+export default page
