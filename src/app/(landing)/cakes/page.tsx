f101f819ac70aba45e6965d6198cd85e570fd522
chiefEbube <ebubechi.anyanwu@yahoo.com>
2025-03-11 13:52:00 +0100

    fixes ui

--- src/app/(landing)/cakes/page.tsx
+++ src/app/(landing)/cakes/page.tsx
@@ -3,10 +3,8 @@ import Cakes from "@/public/images/cakes-bg.jpeg"
 import {
   Card,
   CardContent,
-  CardFooter,
-  CardHeader,
 } from "@/components/ui/card"
-import { Button } from "@/src/components/ui/button"
+import Link from "next/link"
 
 interface CupcakeItem {
   id: string
@@ -84,34 +82,34 @@ const CakesPage = () => {
 
   return (
     <div>
-      <div className="h-[20vh] md:h-[60vh] max-h-[444px] flex items-center justify-center text-white font-nowy"
+      <div className="h-[20vh] md:h-[60vh] max-h-[444px] flex items-center justify-center text-white font-nowy px-4"
         style={{
           backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${Cakes.src})`,
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center",
           backgroundSize: "cover"
         }}>
-        <h1 className="text-[3.5rem] font-semibold w-full md:w-[65%] max-w-4xl text-center">Scroll down and browse through our <span className="text-primary-1100">amazing</span> menu</h1>
+        <h1 className="text-2xl sm:text-4xl lg:text-[3.5rem] font-semibold w-full md:w-[65%] max-w-4xl text-center">Scroll down and browse through our <span className="text-primary-1100">amazing</span> menu</h1>
       </div>
 
-      <div className="container mx-auto py-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-28 px-4">
+      <div className="container mx-auto py-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-6 sm:mt-12 md:mt-20 lg:mt-28 px-4">
         {
           cupcakes.map(cupcake => (
-            <Card key={cupcake.id} className="overflow-hidden border-none shadow-sm p-0">
-              <CardHeader className="p-0">
-                <div className="aspect-square relative bg-primary-300">
-                  <Image src={cupcake.image || "/placeholder.svg"} alt={cupcake.title} fill className="object-cover" />
+            <Card key={cupcake.id} className="h-96 md:h-[500px] border-none shadow-sm p-0 rounded-b-none">
+              <CardContent className="text-center p-0 h-full">
+                <div className="bg-primary-300 w-full h-3/5 flex justify-center items-center">
+                  <div className="w-52 h-52 relative">
+                    <Image src={cupcake.image || "/placeholder.svg"} alt={cupcake.title} fill className="object-center" />
+                  </div>
                 </div>
-              </CardHeader>
-              <CardContent className="text-center">
-                <h3 className="font-semibold text-lg mb-2">{cupcake.title}</h3>
-                <p className="text-sm text-gray-600 mb-4">{cupcake.description}</p>
+                <div className="p-2 h-2/5 flex flex-col items-center justify-center">
+                  <h3 className="font-semibold text-lg font-nowy">{cupcake.title}</h3>
+                  <p className="text-sm text-gray-600">{cupcake.description}</p>
+                  <Link href="#" className="text-primary-1100 underline text-sm font-semibold mt-2">
+                    Order now
+                  </Link>
+                </div>
               </CardContent>
-              <CardFooter className="flex justify-center">
-                <Button variant="link" className="text-pink-600 border-pink-600 hover:bg-pink-50">
-                  Order now
-                </Button>
-              </CardFooter>
             </Card>
           ))
         }
