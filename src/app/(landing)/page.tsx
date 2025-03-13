cefc0278a584efc12e2f6872b4c3fa14a889958c
maetheartist <mariamkosemani1@gmail.com>
2025-03-13 16:42:21 +0100

    fix the contact us page.

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,11 +1,12 @@
+import Contact from "@/src/components/contact-us/Contact";
 import HeroSection from "@/src/components/hero-section";
 
 export default function Home() {
   return (
     <>
-    <HeroSection/>
+    {/* <HeroSection/> */}
     
-    
+    <Contact/>
     </>
     
   );
